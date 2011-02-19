dbb5ec188cd17a8c0451e0e785ac4b7e71a8b1d5
Nick Vlku <nick.vlku@gmail.com>
2011-02-19 13:04:04 -0500

    Documentation in line for the client

--- troveclient/Objects.py
+++ troveclient/Objects.py
@@ -6,16 +6,24 @@ class Query:
         self.services = None
         self.content_types = []
         self.tags_optional = []
+        #: A list of required tags
         self.tags_required = []
+        #: Date "from" as a :class:`datetime.datetime` object
         self.date_from = None
+        #: Date "to" as a :class:`datetime.datetime` object
         self.date_to = None
         self.force_update = False
         self.attributes = {}
+        #: Page number for query
         self.page = 1
+        #: Count for query
         self.count = 50
         self.geo = {} 
+        #: Sort field for query.  Default is timestamp descending
         self.order_by = []       
+        #: A list of identity ids (from :func:`~troveclient.TroveAPI.get_user_info`) that you are querying for.  Default is ALL.
         self.identities_by_id = []
+        #: A request for a specific asset with specific Trove ID.  This causes all other parameters to be ignored.
         self.trove_id = None
         
     def add_geo_bounds(self, lower_left_coords, upper_right_coords):
@@ -29,15 +37,33 @@ class Query:
                         }
     
     def add_order_by(self, value):
+        """ This controls the sort for a query.   The query is first executed by trove, and then sorted.
+        
+            :param value: A string representing the field.  Strings prefixed with a **-** are considered descending.  The default value is *-service_date*                
+        """
+
         self.order_by.append(value)
 
     def remove_order_by(self, value):
+        """ Removes an order_by from the list.  
+            
+            :param value: A string representing the field you'd like to remove from the sort.   
+        """
+
         self.order_by.remove(value)
-    
+
     def add_identity_id(self, id):
-        self.identities_by_id.append(id)
+        """ Adds a specific identity to the query.  The default is empty (which means ALL identities you have access to.)
         
+            :param id: An identity id from the Trove id.
+        """
+        self.identities_by_id.append(id)
+
     def remove_identity_id(self, id):
+        """ Removes a specific identity to the query.  The default is empty (which means ALL identities you have access to.)
+        
+            :param id: An identity id from the Trove id.
+        """        
         self.identities_by_id.remove(id)
 
     def add_content_type(self, id):
@@ -49,16 +75,28 @@ class Query:
 
     @classmethod
     def get_photo_by_trove_id(cls,id):
+        """ A convenience method to load a specific single image from Trove by ID.
+            
+            :param id: The trove id of the specific item you want to pull (Note:  It has to be owned by the user.)
+        """
+
         q = Query()
         q.trove_id = id
         return q
     
 class Result:
+    """ 
+        This is returned by a query to Trove 
+    """
     
     def __init__(self):
+        #: Total number of results for a query on Trove's systems
         self.total_number_of_results = 0
+        #: The number of results returned in this instance (*50* out of 450)
         self.count = 0
+        #: The page number of results.  E.g., if page number 2 and count 50, this would be results 50-100.
         self.page = 1
+        #: The actual objects of returned.  Typically an object like :class:`~troveclient.Objects.Photo`.
         self.objects = []
        
     def __str__(self):
@@ -69,23 +107,41 @@ class Result:
 
     
 class Photo:
-    
+    """ 
+        Represents a photo object returned from Trove
+    """
     def __init__(self):
+        #: The service this photo is from.
         self.service = ""
+        #: The title of this photo
         self.title = ""
+        #: The account that owns it
         self.owner = ""
+        #: A description or summary of the photo
         self.description = ""
+        #: The service\'s ID of the photo (not the Trove ID)  *REQUIRED FIELD*
         self.id = ""
-        self.urls = {}  # thumbnail, large  //tn:48,96,192  Original is required
-        self.tags = []
+        #: A dict of URLs.  All photos have at least "original", but most have thumbnail too.  *REQUIRED FIELD*
+        self.urls = {}  # thumbnail, large  //tn:48,96,192  original is required
+        #: Keywords for a photo
+        self.tags = [] 
+        #: Date photo was created in service.  *REQUIRED FIELD*
         self.date = datetime.datetime.now()
+        #: An album or set id
         self.album_id = ""
+        #: The license of this photo
         self.license = ""
+        #: Photo height
         self.height = 0
+        #: Photo width
         self.width = 0
+        #: Geo coordinates for photo.  Either none or contains a [Latitude, Longitiude]
         self.loc = None
+        #: Is this photo public or private?
         self.public = False
+        #: The original web page context for this photo from the service.
         self.original_web_url = ""        
+        #: The internal unique Trove ID for this photo
         self.trove_id = ""
 
     def __str__(self):
--- troveclient/__init__.py
+++ troveclient/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''A Trove Client API interface using OAuth
-'''
+"""    A Trove Client API interface using OAuth
+"""
 __all__ = ('TroveAPI', 'TroveError',
         'get_request_token', 'get_authorization_url', 'get_access_token','get_photos', 'push_photos', '__version__')
 __author__ = 'Nick Vlku <n =at= yourtrove.com>'
 __status__ = "Beta"
 __dependencies__ = ('python-dateutil', 'simplejson', 'urllib', 'urllib2', 'oauth')
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 # This code is lovingly crafted in Brooklyn, NY (40°42′51″N, 73°57′12″W)
 #
@@ -74,6 +74,8 @@ def _oauth_version():
     return '1.0'
 
 class TroveError():
+    """The :class:`~troveclient.TroveError` is returned when the client receives back an error. It contains a standard http_error object
+    """
     def __init__(self, http_error, request):
         self.http_error = http_error
         self.request = request        
@@ -81,7 +83,9 @@ class TroveError():
     def __repr__(self):
         return "<TroveError with Error Code: \"%s-%s\" and Body: \"%s\">" % (self.http_error.code, self.http_error.msg, self.http_error.read())
     
-class RequiresAcessTokenError():
+class RequiresAccessTokenError():
+    """The :class:`~troveclient.RequiresAccessTokenError` is returned when a call is attempted that requires to be authenticated
+    """
     def __init__(self):
         pass
 
@@ -89,6 +93,8 @@ class RequiresAcessTokenError():
         return "<RequiresAccessTokenError>"
     
 class LocalError():
+    """:class:`~troveclient.LocalError` is returned when a call is made with invalid or missing parameters
+    """
     def __init__(self, msg):
         self.msg = msg
         
@@ -97,6 +103,14 @@ class LocalError():
 
 
 class TroveAPI():
+    """The base class for all Trove client actions
+    
+    :param consumer_key: A string of your application's Trove key
+    :param consumer_secret:  A string of your application's Trove secret
+    :param scope: A list of strings for scopes of the client.  Currently only 'photos' is valid.
+    :param access_token: An instance of :class:`~oauth.oauth.OAuthToken' that is used to make requests on behalf of a user
+    """
+
     DEBUG = False
 
     def __init__(self, consumer_key, consumer_secret, scope=[], access_token=None):
@@ -125,7 +139,7 @@ class TroveAPI():
     def set_user_agent(self, user_agent):
         self._useragent = user_agent
     
-    def get_default_oauth_params(self):
+    def __get_default_oauth_params(self):
         parameters = self._default_oauth_params.copy()
         parameters['oauth_nonce'] = _generate_nonce()
         parameters['oauth_timestamp'] = str(int(time.time()))
@@ -134,7 +148,7 @@ class TroveAPI():
     
     def __make_oauth_request(self, url, parameters=None, oauth_signature=None, token=None, signed=False, method="POST"):
         if parameters is None:
-            parameters = self.get_default_oauth_params()
+            parameters = self.__get_default_oauth_params()
         if token is not None:
             parameters['oauth_token'] = token.key
         if signed:
@@ -170,24 +184,21 @@ class TroveAPI():
         except HTTPError, e:
             error = TroveError(e, request)
             raise error
-        
+    
     def get_request_token(self):
+        """    Returns an instance of :class:`~oauth.oauth.OAuthToken` representing a Request Token for user. 
+        """    
         response = self.__make_oauth_request(REQUEST_TOKEN_URL, oauth_signature=self._Consumer.secret+"&", method="GET")
         self.response = response
         request_token = oauth.OAuthToken.from_string(response.read())
         return request_token
 
-    def get_login_via_service_url(self):
-        url = LOGIN_VIA_SERVICE_URL + "?consumer_key=" + self._Consumer.key
-        request = self._urllib.Request(url)
-        request.add_header('User-agent', self._useragent)
-        if self.DEBUG:
-            print url + '?' + encoded_params
-            raw_input()
-        response = self._urllib.urlopen(request)
-        return response
-            
     def get_authorization_url(self, request_token, callback=None):
+        """      Returns an authorization URL that should be passed to the end user to authorize their request token.
+                
+                :param request_token: An instance of :class:`~oauth.oauth.OAuthToken` that is used to authorize an access token by the user
+                :param callback: A URL for the service to bounceback to.  The default is the application's callback URL specified in Trove
+        """
         parameters = { 'oauth_token': request_token.key }
         
         if callback:
@@ -197,6 +208,10 @@ class TroveAPI():
         return response.geturl()
                 
     def get_access_token(self, request_token):
+        """      Returns an instance of :class:`~oauth.oauth.OAuthToken` that is used to make requests on behalf of a user
+
+                :param request_token: An instance of :class:`~oauth.oauth.OAuthToken` that is used to authorize an access token by the user
+        """
         response = self.__make_oauth_request(ACCESS_TOKEN_URL, oauth_signature='%s&%s' % (self._Consumer.secret, request_token.secret), token=request_token, method='GET')
         self.response = response
         access_token = oauth.OAuthToken.from_string(response.read())
@@ -204,16 +219,27 @@ class TroveAPI():
         return access_token
         
     def get_user_info(self):
+        """     Gets meta information about a User.  *This call requires an access token.*
+        
+                Returns a map of User Metainformation, including their name, email address and identities for scope you've initialized this client for.  
+                User's will have the ability to restrict what is being sent back, so you make no assumptions.
+        """
+
         if self._access_token is None:
-            raise RequiresAcessTokenError()
+            raise RequiresAccessTokenError()
         response = self.__make_oauth_request(USER_INFO_URL, token=self._access_token, signed=True)
         return simplejson.loads(response.read())
 
     def get_photos(self,query=None): 
+        """     Retrieves a user's photos based on the Query passed in. *This call requires an access token.*
+                Returns a :class:`~troveclient.Objects.Result` object that contains the results of your query
+                
+                :param query: An instance of :class:~`troveclient.Objects.Query~ object.  If none is passed, assumes page 1 with a count of 50.
+        """
         if self._access_token is None:
-            raise RequiresAcessTokenError()
+            raise RequiresAccessTokenError()
         
-        parameters = self.get_default_oauth_params()
+        parameters = self.__get_default_oauth_params()
         base_url = CONTENT_ROOT_URL + 'photos/'
 
         if query is not None:
@@ -228,13 +254,19 @@ class TroveAPI():
         return nice_result
 
     def push_photos(self, user_id,  photos_list= []):
+        """     Pushes a list of photos for user_id specified into Trove associated to your service. *This call requires an access token.*
+                Returns a map of the status of your push.
+                
+                :param user_id: The user id of the user on YOUR service.
+                :param photos_list: A list of :class:~`troveclient.Objects.Photo`
+        """
         if self._access_token is None:
-            raise RequiresAcessTokenError()
+            raise RequiresAccessTokenError()
         
         if photos_list is None: 
             return
         
-        parameters = self.get_default_oauth_params()
+        parameters = self.__get_default_oauth_params()
 
         json_photos_list = simplejson.dumps(photos_list, cls=JSONFactories.encoders.get_encoder_for(photos_list[0]))
                                             
@@ -247,22 +279,33 @@ class TroveAPI():
         return simplejson.loads(response.read())
         
     def get_services(self):
+        """     Gets all the services Trove provides for the scope set for this client.  *This call requires an access token.*
+                Returns a list of service names
+        """        
         if self._access_token is None:
-            raise RequiresAcessTokenError()
+            raise RequiresAccessTokenError()
 
         response = self.__make_oauth_request(ADD_URLS_FOR_SERVICES_URL, token=self._access_token, signed=True)
         return simplejson.loads(response.read()).keys()    
     
     def get_url_for_service(self, service, redirect_url=None):
+        """     This creates a URL that allows a user to add a service to their Trove and then immediately bounce back to your service.  If a 
+                redirect_url URL is specified it uses that, otherwise it uses your application's default.  *This call requires an access token.*
+                Returns a one-time use URL for the user
+                
+                :param service: The service name, usually from the list returned by get_services
+                :param redirect_url: A redirect URL override.  The default is your application's callback.
+        """        
+
         if self._access_token is None:
-            raise RequiresAcessTokenError()
+            raise RequiresAccessTokenError()
 
         response = self.__make_oauth_request(ADD_URLS_FOR_SERVICES_URL, token=self._access_token, signed=True)
         services = simplejson.loads(response.read())
             
         if service in services.keys():
             service_url = API_BETA_BASE + services[service]
-            parameters = self.get_default_oauth_params() 
+            parameters = self.__get_default_oauth_params() 
             if redirect_url is not None:
                 parameters['redirect_url'] = redirect_url
 
